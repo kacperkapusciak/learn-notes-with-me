3318c89a6781d42a9e4ab099ed48444f5686af17
kacperkapusciak <kapusciak.k@gmail.com>
2022-01-08 20:02:41 +0100

    Run test on CI

--- src/lesson/lesson.controller.spec.ts
+++ src/lesson/lesson.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LessonController } from './lesson.controller';
+import { LessonService } from './lesson.service';
 
 describe('LessonController', () => {
   let controller: LessonController;
 
+  const service: Partial<LessonService> = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LessonController],
+      providers: [
+        {
+          provide: LessonService,
+          useValue: service,
+        },
+      ],
     }).compile();
 
     controller = module.get<LessonController>(LessonController);
