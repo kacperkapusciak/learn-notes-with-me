cf8c445c80e152d57c9a3307bf941c0d90dd9330
kacperkapusciak <kapusciak.k@gmail.com>
2022-01-08 21:26:45 +0100

    Add send invalid request button

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+import axios from '../api/instance';
 
 import Button from '../components/Button';
 import FullPageCenter from '../components/FullPageCenter';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Spacer = styled.div`
+  margin: 30px;
+`;
+
 function Home() {
   const navigate = useNavigate();
 
+  const sendInvalidRequest = async () => {
+    try {
+      await axios.get('/test');
+    } catch (e) {
+      // pass
+    }
+  };
+
   return (
     <FullPageCenter>
-      <Button onClick={() => navigate('/learn')}>Start a lesson</Button>
+      <Wrapper>
+        <Button onClick={() => navigate('/learn')}>Start a lesson</Button>
+        <Spacer />
+        <Button onClick={sendInvalidRequest}>Send invalid request</Button>
+      </Wrapper>
     </FullPageCenter>
   );
 }
