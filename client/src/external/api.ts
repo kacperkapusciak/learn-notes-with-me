90827b7372bb15aaa30e05b0b665785d14501590
kacperkapusciak <kapusciak.k@gmail.com>
2022-01-10 22:31:53 +0100

    Use custom env variable for frontend

--- client/src/external/api.ts
+++ client/src/external/api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const baseURL = () => {
-  switch (process.env.NODE_ENV) {
+  switch (process.env.REACT_APP_ENVIRONMENT) {
     case 'production':
       return 'https://learn-notes-with-me.azurewebsites.net/api/';
-    case 'test':
+    case 'staging':
       return 'https://learn-notes-with-me-staging.azurewebsites.net/api/';
     case 'development':
+    default:
       return 'http://localhost:3000/api/';
   }
 };
