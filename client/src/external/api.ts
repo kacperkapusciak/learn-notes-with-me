08ab9b658b9aa12fc16eb0e18a92f1b98f868b9b
kacperkapusciak <kapusciak.k@gmail.com>
2022-01-10 22:09:41 +0100

    Change backend url per env

--- client/src/external/api.ts
+++ client/src/external/api.ts
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://learn-notes-with-me.azurewebsites.net/api/'
-    : 'http://localhost:3000/api/';
+const baseURL = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return 'https://learn-notes-with-me.azurewebsites.net/api/';
+    case 'test':
+      return 'https://learn-notes-with-me-staging.azurewebsites.net/api/';
+    case 'development':
+      return 'http://localhost:3000/api/';
+  }
+};
 
 export const api = axios.create({
-  baseURL,
+  baseURL: baseURL(),
 });
